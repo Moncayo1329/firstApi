d51fa3fcb60134e57bdc16f2f4e2ec24534018a7
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-10-28 19:11:19 +0000

    31.36

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req,res) => {
-console.log('request received')
-res.end('hola mundo everyone fuck ')
+const processRequest = (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Charset', 'utf-8')
+        res.end('Bienvenido a mi pagina de inicio');
+    }
+    else if(req.url === '/contacto') {
+   res.statusCode = 200 
+   res.end('<h1> Conctacto </h1>')
+    }
 
-})
+    else {
+ res.statusCode = 404 // not found
+ res.end('<h1>404</h1>')
+
+    }
+
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
 
